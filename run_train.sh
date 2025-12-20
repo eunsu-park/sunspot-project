6a4d02f58928891900fbcea5e800d760bd9618b1
Eunsu Park <phd.choux@gmail.com>
2025-12-19 22:34:38 -0500

    Create run_train.sh

---
+++ run_train.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -l
+
+#SBATCH --job-name=SUNSPOT_TRAIN
+#SBATCH --output=/Users/eunsupark/ap/renew/validation_outs/%x.%j.out
+#SBATCH --error=/Users/eunsupark/ap/renew/validation_errs/%x.%j.err
+#SBATCH --partition=gpu
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=8
+#SBATCH --gres=gpu:1
+#SBATCH --mem=8000M
+#SBATCH --qos=standard
+#SBATCH --account=wangj
+#SBATCH --time=3-00:00:00
+
+module purge > /dev/null 2>&1
+module load wulver # Load slurm, easybuild
+conda activate ap
+/home/hl545/miniconda3/envs/sunspot/bin/python train.py --config-name wulver
